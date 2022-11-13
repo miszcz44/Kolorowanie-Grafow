d2da69a518df005d446c12be448e6a96541d27fc
mckmck0 <104257821+mckmck0@users.noreply.github.com>
2022-11-14 00:04:12 +0100

    Update main.py
    Readable variables

--- main.py
+++ main.py
@@ -1,34 +1,34 @@
-def KolorujZachlannie(macierz):
-	n = len(macierz)
+def colorGreedy(matrix):
+	n = len(matrix)
 	tab = [-1] * n
 	tab[0] = 0
-	kolory = [False] * n
+	colors = [False] * n
 	for i in range(n):
 		for j in range(n):
-			kolory[j] = False
+			colors[j] = False
 		for j in range(n):
-			if (macierz[i][j] > 0 and tab[j] != -1):
-				kolory[tab[j]] = True
+			if (matrix[i][j] > 0 and tab[j] != -1):
+				colors[tab[j]] = True
 		k = 0
 		while kolory[k]:
 			k += 1
 		tab[i] = k
 	return tab
 
-plik = open("algor.txt", "r")
+file = open("algor.txt", "r")
 
-liczbaKrawędzi = int(plik.readline())
+edge_number = int(file.readline())
 
-macierz = [[ 0 for i in range(liczbaKrawędzi) ] for j in range(liczbaKrawędzi) ]
+matrix = [[ 0 for i in range(edge_number) ] for j in range(edge_number) ]
 
-for liniaPliku in plik:
-	sąsiadująceKrawędzie = liniaPliku.split(" ")
-	sąsiadująceKrawędzie[1] = sąsiadująceKrawędzie[1][:len(sąsiadująceKrawędzie[1]) - 1]
-	# print(sąsiadująceKrawędzie)
-	macierz[int(sąsiadująceKrawędzie[0])-1][int(sąsiadująceKrawędzie[1])-1] = 1
-	macierz[int(sąsiadująceKrawędzie[1])-1][int(sąsiadująceKrawędzie[0])-1] = 1
+for line in file:
+	neighbouring_edges = line.split(" ")
+	neighbouring_edges[1] = neighbouring_edges[1][:len(neighbouring_edges[1]) - 1]
+	# print(neighbouring_edges)
+	matrix[int(neighbouring_edges[0])-1][int(neighbouring_edges[1])-1] = 1
+	matrix[int(neighbouring_edges[1])-1][int(neighbouring_edges[0])-1] = 1
 
-tab = KolorujZachlannie(macierz)
+tab = colorGreedy(macierz)
 print("Węzeł\tKolor")
-for i in range(0, liczbaKrawędzi):
+for i in range(0, edge_number):
     print(str(i + 1) + "\t" + str(tab[i]))
